cd9db4e0b97a98107ebb78e8c612b884912590ec
Przemysław Jabłoński <sharaquss@gmail.com>
2016-02-11 20:50:58 +0100

    READFROMFILE

--- HashCode2016/src/Warehouse.java
+++ HashCode2016/src/Warehouse.java
@@ -1,26 +1,49 @@
+import java.util.LinkedList;
 
 public class Warehouse {
 
-Warehouse(Coordinates cord, int ID){
-	this.cord = new Coordinates(cord.getr(),cord.getc());
-	setID(ID);
-}
+	private int ID;
+	private Coordinates cord;
+	private LinkedList<Integer> availableProducts;
+
+
+	Warehouse(Coordinates cord, int ID){
+		this.cord = new Coordinates(cord.getr(),cord.getc());
+		this.ID = ID;
+
+		availableProducts = new LinkedList<>();
+		for (int i=0; i < GlobalData.productTypeSize; ++i)
+			availableProducts.add(0);
+	}
+
+	public Coordinates getCord() {
+		return cord;
+	}
+
+	public void setCord(Coordinates cord) {
+		this.cord = cord;
+	}
+
+	public LinkedList<Integer> getAvailableProducts() {
+		return availableProducts;
+	}
+
+	public void setAvailableProducts(LinkedList<Integer> availableProducts) {
+		this.availableProducts = availableProducts;
+	}
+
+	public void setAvailableProductType(int count, int productType){
+		this.availableProducts.set(productType, count);
+	}
+
+	public int getID(){
+		return ID;
+	}
+
+	public void setID(int id){
+		this.ID=id;
+	}
 
 
 
-
-public int getID(){
-	return ID;
-}
-
-public void setID(int id){
-	this.ID=id;
-}
-
-
-
-
-
-private int ID;	
-private Coordinates cord;
 }
--- HashCode2016/src/test.java
+++ HashCode2016/src/test.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class test {
 
@@ -21,6 +24,67 @@ public class test {
 				printCommand();
 			}
 		}
+	}
+
+	private static void readFromFile() throws FileNotFoundException {
+		File plik = new File("busy_day.in");
+		Scanner scanner = new Scanner(plik);
+
+		String line = scanner.nextLine();
+
+		Scanner stringScanner = new Scanner(line);
+
+		row = stringScanner.nextInt();
+		columns = stringScanner.nextInt();
+		droneCount = stringScanner.nextInt();
+		turnsCount = stringScanner.nextInt();
+		droneMaxLoad = stringScanner.nextInt();
+
+		stringScanner = new Scanner(scanner.nextLine());
+		productTypeCount = stringScanner.nextInt();
+
+		stringScanner = new Scanner(scanner.nextLine());
+		int i = 0;
+		while(stringScanner.hasNextInt()){
+			productType[i].weight = stringScanner.nextInt();
+			++i;
+		}
+
+		stringScanner = new Scanner(scanner.nextLine());
+		warehouseCount = stringScanner.nextInt();
+
+		for (int w = 0; w < warehouseCount ; ++w) {
+			stringScanner = new Scanner(scanner.nextLine());
+			warehouse[w] = new Warehouse(new Coordinates(stringScanner.nextInt(), stringScanner.nextInt()), w);
+
+			stringScanner = new Scanner(scanner.nextLine());
+			for (int i=0; i<productTypeCount; ++i) {
+				warehouse[w].setcostamprodukt(stringScanner.nextInt(), i);
+			}
+
+		}
+
+		stringScanner = new Scanner(scanner.nextLine());
+		orderCount = stringScanner.nextInt();
+
+		for (int o=0; o <orderCount; ++o) {
+
+			stringScanner = new Scanner(scanner.nextLine());
+			order[o].setDestination(stringScanner.nextInt(), stringScanner.nextInt());
+
+			stringScanner = new Scanner(scanner.nextLine());
+			order[o].setTotalProductCount(stringScanner.nextInt());
+
+
+			for (int i=0 ; i < orderCount ; ++i) {
+				stringScanner = new Scanner(scanner.nextLine());
+
+				order[o].addToListOfProducts(stringScanner.nextInt());
+			}
+
+		}
+
+
 	}
 
 }
